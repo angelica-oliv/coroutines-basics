4c22c9925aabf2e56e4d3d4cd2db8b1850ba4a89
angelica <angelica.liv@gmail.com>
2019-05-25 18:22:51 -0300

    First commit.

---
+++ src/main/kotlin/com/angelicao/coroutines/scope/GlobalScopeExample.kt
@@ -0,0 +1,16 @@
+package com.angelicao.coroutines.scope
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    GlobalScope.launch {
+        repeat(1000) { i ->
+            println("I'm sleeping $i ...")
+            delay(500L)
+        }
+    }
+    delay(1300L) // just quit after delay
+}
